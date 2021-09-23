96cbad5fdcaf3eb0f37d61894742b94088973674
Hamit SEYREK <hamitseyrek@gmail.com>
2021-09-23 13:10:19 +0300

    generate listvies.builder

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -15,13 +15,20 @@ class _MyHomePageState extends State<MyHomePage>
   bool _openMenu = false;
   AnimationController? _controller;
   Animation<double>? _scaleAnimation;
-  final Duration _duration = const Duration(microseconds: 5000);
+  Animation<double>? _scaleMenuAnimation;
+  Animation<Offset>? _menuOffsetAnimation;
+  final Duration _duration = const Duration(milliseconds: 500);
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(vsync: this, duration: _duration);
     _scaleAnimation = Tween<double>(begin: 1.0, end: 0.6).animate(_controller!);
+    _scaleMenuAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_controller!);
+    _menuOffsetAnimation = Tween<Offset>(
+            begin: const Offset(-1, 0), end: const Offset(0, 0))
+        .animate(CurvedAnimation(parent: _controller!, curve: Curves.easeIn));
   }
 
   @override
@@ -48,41 +55,47 @@ class _MyHomePageState extends State<MyHomePage>
   }
 
   Widget createMenu(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 16.0),
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Dashboard',
-              style: _menuFontStyle,
+    return SlideTransition(
+      position: _menuOffsetAnimation!,
+      child: ScaleTransition(
+        scale: _scaleMenuAnimation!,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Dashboard',
+                  style: _menuFontStyle,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Messages',
+                  style: _menuFontStyle,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Utility Bills',
+                  style: _menuFontStyle,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Branches',
+                  style: _menuFontStyle,
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Messages',
-              style: _menuFontStyle,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Utility Bills',
-              style: _menuFontStyle,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Branches',
-              style: _menuFontStyle,
-            ),
-          ],
+          ),
         ),
       ),
     );
@@ -102,65 +115,92 @@ class _MyHomePageState extends State<MyHomePage>
               _openMenu ? const BorderRadius.all(Radius.circular(20)) : null,
           elevation: 8,
           color: const Color(0xFF343442),
-          child: Container(
-            padding: const EdgeInsets.only(left: 16, right: 16, top: 8),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          if (_openMenu) {
-                            _controller!.reverse();
-                          } else {
-                            _controller!.forward();
-                          }
-                          _openMenu = !_openMenu;
-                        });
-                      },
-                      child: const Icon(
-                        Icons.menu,
-                        color: Colors.white,
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 8),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            if (_openMenu) {
+                              _controller!.reverse();
+                            } else {
+                              _controller!.forward();
+                            }
+                            _openMenu = !_openMenu;
+                          });
+                        },
+                        child: const Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    const Text(
-                      'My Cards',
-                      style: TextStyle(color: Colors.white, fontSize: 24),
-                    ),
-                    const Icon(Icons.add),
-                  ],
-                ),
-                Container(
-                    margin: const EdgeInsets.only(top: 15),
-                    height: 200,
-                    child: PageView(
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        Container(
-                          color: Colors.red,
-                          width: 100,
-                          margin: const EdgeInsets.symmetric(horizontal: 12),
-                        ),
-                        Container(
-                          color: Colors.blue,
-                          width: 100,
-                          margin: const EdgeInsets.symmetric(horizontal: 12),
-                        ),
-                        Container(
-                          color: Colors.amber,
-                          width: 100,
-                          margin: const EdgeInsets.symmetric(horizontal: 12),
-                        ),
-                        Container(
-                          color: Colors.green,
-                          width: 100,
-                          margin: const EdgeInsets.symmetric(horizontal: 12),
-                        ),
-                      ],
-                    )),
-              ],
+                      const Text(
+                        'My Cards',
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                      const Icon(Icons.add),
+                    ],
+                  ),
+                  Container(
+                      margin: const EdgeInsets.only(top: 15),
+                      height: 200,
+                      child: PageView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          Container(
+                            width: 100,
+                            margin: const EdgeInsets.symmetric(horizontal: 12),
+                            decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(10)),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(10)),
+                            width: 100,
+                            margin: const EdgeInsets.symmetric(horizontal: 12),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.green,
+                                borderRadius: BorderRadius.circular(10)),
+                            width: 100,
+                            margin: const EdgeInsets.symmetric(horizontal: 12),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.amber,
+                                borderRadius: BorderRadius.circular(10)),
+                            width: 100,
+                            margin: const EdgeInsets.symmetric(horizontal: 12),
+                          ),
+                        ],
+                      )),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ListView.builder(
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: const Icon(Icons.access_alarm),
+                        title: Text('Person $index'),
+                        trailing: const Icon(Icons.format_align_center),
+                        tileColor: Colors.white,
+                      );
+                    },
+                    shrinkWrap: true,
+                    itemCount: 51,
+                    scrollDirection: Axis.vertical,
+                    physics: const BouncingScrollPhysics(),
+                  )
+                ],
+              ),
             ),
           ),
         ),
